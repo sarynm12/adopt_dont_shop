f92350ec091baf55ee371d5731abaa357ade9ea2
Saryn Mooney <smooney15@my.whitworth.edu>
2020-08-19 22:39:11 -0600

    added update to routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   get '/shelters/new', to: 'shelters#new'
   post '/shelters', to: 'shelters#create'
   get '/shelters/:id', to: 'shelters#show'
-  get '/tasks/:id/edit', to: 'tasks#edit'
-  patch '/tasks/:id', to: 'tasks#update'
+  get '/shelters/:id/edit', to: 'shelters#edit'
+  patch '/shelters/:id', to: 'shelters#update'
 end
